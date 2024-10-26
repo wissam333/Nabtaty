03b5c748da564ffd88dcbb50a017a1fa58e7a91e
Wissam_Najjom <wissam.n.najjom@gmail.com>
2024-10-26 18:08:45 +0300

    notes 2024/10/26

---
+++ .history/components/Shared/Footer/CopyRight_20241026161142.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="c">
+    <div>
+      <ul>
+        <li>
+          <div>
+            <nuxt-link to="">
+              {{ $i18n.locale === "ar" ? "الرئيسية" : "Home" }}
+            </nuxt-link>
+          </div>
+        </li>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
+    </div>
+  </div>
+</template>
+<script setup></script>
+<style lang="scss" scoped>
+.c {
+  color: #fff;
+  font-size: 18px;
+  text-align: center;
+  padding-bottom: 1px;
+  margin: 0px 0px 0px 0px;
+  padding-top: 10px;
+  background: linear-gradient(to left, $main, $second, $main);
+
+  @media (max-width: 768px) {
+    width: 100vw;
+    margin: 60px 0px 0px 0px;
+    font-size: 15px;
+  }
+
+  .name {
+    color: #fff;
+    font-weight: bold;
+  }
+}
+</style>
